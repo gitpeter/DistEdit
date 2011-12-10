25609da9c14cce3f37d63cf46b8d742785d94808
Peter Votzi <petervotzi@gmail.com>
2011-12-10 20:19:13 +0100

    Initalized Rails 3.1.3 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DistEdit::Application.config.secret_token = '95a4b27cdcedd42476831bb893985eff828acdfb044b367aca1925132e4a4a673b45905f45d1cdee987ca06997a7d15ab6c585643c6458eb8dc1c963faf35e29'
